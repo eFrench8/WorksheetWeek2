d1755aedcfdf0fe46378d4265af8eda01b57c55b
eFrench8 <164631948+eFrench8@users.noreply.github.com>
2024-10-16 16:25:45 +0000

    fixing task c again

--- task_c.py
+++ task_c.py
@@ -1,10 +1,11 @@
 password = list(input("Please enter a password "))
 
-hasInt = any(isinstance(item, int) for item in password)
-hasStr = any(isinstance(item, str) for item in password)
+hasInt = any(char.isalnum() for char in password)
+hasStr = any(char.isalpha() for char in password)
 
+validator = hasInt and hasStr
 
-if len(password) >= 7 and hasInt == True and hasStr == True:
+if len(password) > 7 and validator == True:
     print("Your password is valid")
 else:
     print("Your password must contain at least 8 characters, and a mix of letters and numbers")
